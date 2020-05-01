fd5a36f9e610fe45756661a2cb350771652e3250
Francesco Zimbolo <fra.zimbolo@stud.uniroma3.it>
2020-05-01 19:47:10 +0000

    User Class further development * Altered some docstrings * Added inline comments * Added state support on __init__ * Added support for passing both the user class and a telegram id
    Signed-off-by: Francesco Zimbolo <dtrandom747@gmail.com>

--- source/objects/user.py
+++ source/objects/user.py
@@ -1,9 +1,9 @@
-import redis
-from source.objects.config_parser import ConfigParser
-from botogram import User as bUser
-from typing import Union
-from datetime import datetime as dt
-config = ConfigParser("data/configs/config.json")
+import redis                                            # Required to work with redis
+from source.objects.config_parser import ConfigParser   # Configuration parser to get redis db info
+from botogram import User as bUser                      # Get a botogram.User class and save it as bUser
+from typing import Union                                # Needed for parameters and return hints
+from datetime import datetime as dt                     # Needed to save the last activity
+config = ConfigParser("data/configs/config.json")       # Load the config file and parse it
 
 r = redis.Redis(host=config.redis.IP, db=config.redis.DATABASE, port=config.redis.PORT, password=config.redis.PASSWORD,
                 decode_responses=True)
@@ -47,33 +47,56 @@ class User:
         Raises
         ------
         ValueError
-            If neither the botogram_user nor telegram_id is provided
+            If neither the botogram_user nor telegram_id is provided or if both are provided and don't match
         """
 
         if not all((botogram_user, telegram_id)):           # If both arguments are left to be default
             raise ValueError("Expected either botogram_user or telegram_id values")  # Raise a ValueError
             #                                                                          (at least one is required)
 
-        if botogram_user and not telegram_id:               # If a botogram User is passed
+        if botogram_user and not telegram_id or botogram_user.id == telegram_id:     # If a botogram User is passed
+            #                                                                          or both are passed and they match
             self.id = botogram_user.id                      # Save its id as an attribute
             self.redis_hash = f"user:{self.id}"             # Get it's redis hash from the id
 
-            if not (self._get_redis_value(self.id)):            # If its user data is not present on redis
+            if not (self._get_redis_value("id")):            # If its user data is not present on redis
                 self.first_name = botogram_user.first_name      # Save its first name on tg as an attribute
                 self.last_name = botogram_user.last_name        # Save its last name on tg as an attribute
                 #                                                 (None if not present)
                 self.username = botogram_user.username          # Save its username on tg as an attribute
                 #                                                 (None if not present)
                 self.last_activity = dt.timestamp(dt.now())
+                self._state = "home"
 
                 self._set_redis_value("first_name", self.first_name)    # Set its first name on redis
                 self._set_redis_value("last_name", self.last_name)      # Set its last name on redis
                 self._set_redis_value("username", self.username)        # Set its username on redis
+                self._set_redis_value("last_activity", self.last_activity)  # Set its last activity on redis
+                self.state(self._state)                                  # Save the user state
 
-            else:
-                self.first_name = self._get_redis_value("first_name")
+            else:           # If the user is present on redis
+                self.first_name = self._get_redis_value("first_name")       # Get its info
                 self.last_name = self._get_redis_value("last_name")
                 self.username = self._get_redis_value("username")
+                self.last_activity = self._get_redis_value("last_activity", float)
+                self._state = self.state()
+
+        elif telegram_id and not botogram_user:     # If a telegram id is passed
+            self.id = telegram_id                   # Save it as an attribute
+            self.redis_hash = f"user:{self.id}"     # Get the redis hash
+
+            if not(self._get_redis_value("id")):    # Check if it's present on redis
+                raise ValueError("User not found in the redis database, cannot utilize the telegram id")
+                # Can't get data from an id, so raise ValueError
+
+            self.first_name = self._get_redis_value("first_name")       # Else get its data
+            self.last_name = self._get_redis_value("last_name")
+            self.username = self._get_redis_value("username")
+            self.last_activity = self._get_redis_value("last_activity", float)
+            self._state = self.state()
+
+        else:
+            raise ValueError("Both a Botogram User and a Telegram ID were passed, but they didn't match")
 
     def _get_redis_value(self, key: str, type_of_return: type = str) -> Union[str, int, float, bool]:
         """Function which simplifies the redis hget function
@@ -109,23 +132,24 @@ class User:
                 raise TypeError("The requested value from Redis is not a float")  # Alert if not possible
         return value
 
-    def _set_redis_value(self, key: str, value: Union[str, int]) -> bool:
+    def _set_redis_value(self, key: str, value: Union[str, int, float]) -> bool:
         """Function which simplifies the redis hset function
         Parameters
         ----------
         key : str
             The key to set on redis
-        value : str or int
+        value : Union[str, int, float]
+            The value to set on that key
 
         Returns
         -------
         bool
-            True if the operation succeeded, False otherwise
+            True if the operation succeeded and a new entry was created, False if an existing key was altered
         """
 
         return r.hset(self.redis_hash, key, value)
 
-    def state(self, new_state: str = ""):
+    def state(self, new_state: str = "") -> Union[str, bool]:
         """Function which sets a new user state or returns the current one if no new one is provided
         Parameters
         ----------
@@ -138,8 +162,8 @@ class User:
             Returns a str representing the user's state or True if the state was set successfully
         """
 
-        if not new_state:
-            return self._get_redis_value("state")
+        if not new_state:                               # If a new state wasn't passed
+            return self._get_redis_value("state")       # Get the current one
 
-        self._set_redis_value("state", new_state)
-        return True
+        self._set_redis_value("state", new_state)       # Otherwise set the new state
+        return True                                     # Return True
