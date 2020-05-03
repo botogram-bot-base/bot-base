39039b6e9e12c6bf6e33108b26686e8f81c08471
matteo <matteo.bocci@gmail.com>
2020-05-03 20:22:16 +0200

    delete useless code from start.py

--- start.py
+++ start.py
@@ -21,9 +21,3 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-from source.objects.language import CallMess
-
-mess = CallMess("home","IT")
-print(mess.message(),
-      mess.callback(),
-      mess.notify())
